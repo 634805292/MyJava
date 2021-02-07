d13074d2fda135aca84baee9da965686230f35ea
zhaokai <634805292@qq.com>
2021-02-07 20:53:53 +0800

    项目初始化

---
+++ Concurrent/src/main/demo01.java
@@ -0,0 +1,14 @@
+package main;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class demo01 {
+
+    public static void main(String[] args) {
+
+        ExecutorService pool = Executors.newFixedThreadPool(2);
+
+    }
+
+}
